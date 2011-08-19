bc95bba920bce3c79e75a255ff0c87e29e64aa33
David Romaní <david@flux.cat>
2011-08-19 08:54:14 +0200

    Contingut de templates_c eliminat

--- web/templates_c/%%27^273^273428E9%%overview.lang.en.html.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2011-08-18 12:34:36
-         compiled from overview.lang.en.html */ ?>
-<ul>
-	<li>Catalogació per grups i etiquetes</li>
-	<li>Previsualització amb miniatura</li>
-	<li>Comparteix amb les xarxes socials</li>
-	<li>Cross-browser &amp; cross-platform compatible</li>
-</ul>
